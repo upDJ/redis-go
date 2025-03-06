a241e98fb94f66502fe4eb6d61f35bcf4dcf3a80
upDJ <deion.shall@gmail.com>
2025-03-06 00:46:57 -0800

    add while loop to simulate multiple pongs

--- app/main.go
+++ app/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 )
 
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")

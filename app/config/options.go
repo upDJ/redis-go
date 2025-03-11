f6a6ef7668ddc5c63fdc9c7e7a827461683eb233
upDJ <deion.shall@gmail.com>
2025-03-11 10:03:05 -0700

    add support for rdb file config with singleton

---
+++ app/config/options.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"flag"
+	"sync"
+)
+
+type Config struct {
+	Dir			string
+	DBFilename	string
+}
+
+var (
+	instance *Config
+	once 	 sync.Once
+)
+
+func GetConfig() *Config {
+	once.Do(func() {
+		instance = &Config{}
+		flag.StringVar(&instance.Dir, "dir", "./", "Path to directory where the RDB file is stored")
+		flag.StringVar(&instance.DBFilename, "dbfilename", "default", "Name of the RDB file")
+		flag.Parse()
+	})
+	return instance
+}

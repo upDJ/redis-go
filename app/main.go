979034cd3bea2a4af4a3af534ee1b4c95c726727
upDJ <deion.shall@gmail.com>
2025-03-11 10:01:40 -0700

    implement config for rdb file support as a singleton

--- app/main.go
+++ app/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/codecrafters-io/redis-starter-go/app/connect"
+	"github.com/codecrafters-io/redis-starter-go/app/config"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-  l := connect.InitTcp()
-  connect.HandleConnection(l)
+	config.GetConfig()
+  	l := connect.InitTcp()
+	connect.HandleConnection(l)
 }
